45da252d6b11749929f0f81ebe5299cb513f7d3f
Olman <Olmancampos07@hotmail.com>
2021-04-30 18:22:59 +0000

    Cambios

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -22,59 +22,61 @@ export const Register = () => {
 	};
 	return (
 		<div className="row justify-content-center" style={registerstyle}>
-			<h1 className="text-center" style={textstyle}>
-				Registro
-			</h1>
-			<div className="ItemComponent">
-				<label className="LabelItemComponent">User</label>
+			<div className="text-center" style={textstyle}>
+				<h1>Registro</h1>
 			</div>
-			<div className="inputContainer">
-				<input id="username" placeholder name="username" type="text"></input>
-			</div>
+			<div className="CreateUserContainer">
+				<div className="ItemComponent">
+					<label className="LabelItemComponent">User</label>
+				</div>
+				<div className="inputContainer">
+					<input id="username" placeholder name="username" type="text"></input>
+				</div>
 
-			<div className="ItemComponent">
-				<label className="LabelItemComponent">First name</label>
-			</div>
-			<div className="inputContainer">
-				<input id="firstname" placeholder name="firstname" type="text"></input>
-			</div>
+				<div className="ItemComponent">
+					<label className="LabelItemComponent">First name</label>
+				</div>
+				<div className="inputContainer">
+					<input id="firstname" placeholder name="firstname" type="text"></input>
+				</div>
 
-			<div className="ItemComponent">
-				<label className="LabelItemComponent">Last name</label>
-			</div>
-			<div className="inputContainer">
-				<input id="lastname" placeholder name="lastname" type="text"></input>
-			</div>
+				<div className="ItemComponent">
+					<label className="LabelItemComponent">Last name</label>
+				</div>
+				<div className="inputContainer">
+					<input id="lastname" placeholder name="lastname" type="text"></input>
+				</div>
 
-			<div className="ItemComponent">
-				<label className="LabelItemComponent">Password</label>
-			</div>
-			<div className="inputContainer">
-				<input id="password" placeholder name="password" type="password"></input>
-			</div>
+				<div className="ItemComponent">
+					<label className="LabelItemComponent">Password</label>
+				</div>
+				<div className="inputContainer">
+					<input id="password" placeholder name="password" type="password"></input>
+				</div>
 
-			<div className="ItemComponent">
-				<label className="LabelItemComponent">Repeat Password</label>
-			</div>
-			<div className="inputContainer">
-				<input
-					id="passwordAgain"
-					placeholder
-					name="passwordAgain"
-					type="password"
-					className="regularStyle"></input>
-			</div>
+				<div className="ItemComponent">
+					<label className="LabelItemComponent">Repeat Password</label>
+				</div>
+				<div className="inputContainer">
+					<input
+						id="passwordAgain"
+						placeholder
+						name="passwordAgain"
+						type="password"
+						className="regularStyle"></input>
+				</div>
 
-			<div className="regularButtonLoginContainer">
-				<button disabled className="regularButtonLoginContainer">
-					Sing Up
-				</button>
-			</div>
+				<div className="regularButtonLoginContainer">
+					<Link to="/">
+						<button className="regularButtonLoginContainer">Sing Up</button>
+					</Link>
+				</div>
 
-			<div className="ItemComponent">
-				<Link to="/login">
-					<label className="LabelItemComponent">I want to Sing In </label>
-				</Link>
+				<div className="ItemComponent">
+					<Link to="/login">
+						<label className="LabelItemComponent">I want to Sing In </label>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
