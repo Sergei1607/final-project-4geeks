2e4302dfedc3a3318a62c85d9956c20f44f3bcd7
migo8591 <mcatedral24@hotmail.com>
2021-04-30 03:38:33 +0000

    Avance del formulario

--- src/front/js/pages/adoptform.js
+++ src/front/js/pages/adoptform.js
@@ -1,11 +1,41 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-import "../../styles/demo.scss";
-
-export const Adoptform = () => {
+export const Adoptform = props => {
 	const { store, actions } = useContext(Context);
-	return "";
+	const params = useParams();
+	return (
+		<div className="container mt-5">
+			<form>
+				<div className="form-group">
+					<label htmlFor="exampleInputEmail1">Email address</label>
+					<input
+						type="email"
+						className="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+						placeholder="Enter email"
+					/>
+					<small id="emailHelp" className="form-text text-muted">
+						your email with anyone else.
+					</small>
+				</div>
+				<div className="form-group">
+					<label htmlFor="exampleInputPassword1">Password</label>
+					<input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+				</div>
+				<div className="form-group form-check">
+					<input type="checkbox" className="form-check-input" id="exampleCheck1" />
+					<label className="form-check-label" htmlFor="exampleCheck1">
+						Check me out
+					</label>
+				</div>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 };
