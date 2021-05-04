e8de338f032615a1c29b4b807257632a941e6a90
migo8591 <mcatedral24@hotmail.com>
2021-05-04 23:21:52 +0000

    avances 2

--- src/front/js/pages/adoptform.js
+++ src/front/js/pages/adoptform.js
@@ -20,63 +20,44 @@ export const Adoptform = () => {
 				<div className="row">
 					<div className="col-3"></div>
 					<div className="col-6 bg-danger">
+						<h1>Formulario de Adopción</h1>
 						<form>
-							<div className="form-group col-md-6">
-								<div className="form-row">
-									<div className="col">
-										<input type="text" className="form-control" placeholder="First name" />
-									</div>
-									<div className="col">
-										<input type="text" className="form-control" placeholder="Last name" />
-									</div>
+							<div className="form-row">
+								<div className="col">
+									<label htmlFor="inputEmail4">Nombre</label>
+									<input type="text" className="form-control" />
 								</div>
-								<div className="form-group col-md-6">
-									<label htmlFor="inputEmail4">Email</label>
-									<input type="email" className="form-control" id="inputEmail4" placeholder="Email" />
-								</div>
-								<div className="form-group col-md-6">
-									<label htmlFor="inputPassword4">Password</label>
-									<input
-										type="password"
-										className="form-control"
-										id="inputPassword4"
-										placeholder="Password"
-									/>
+								<div className="col">
+									<label htmlFor="inputEmail4">Apellidos</label>
+									<input type="text" className="form-control" />
 								</div>
 							</div>
+
 							<div className="form-group">
-								<label htmlFor="inputAddress">Address</label>
+								<label htmlFor="inputAddress">Email</label>
+								<input type="email" className="form-control" id="inputEmail3" placeholder="Email" />
+							</div>
+							<div className="form-group">
+								<label htmlFor="inputAddress2">Dirección</label>
 								<input
 									type="text"
 									className="form-control"
-									id="inputAddress"
-									placeholder="1234 Main St"
-								/>
-							</div>
-							<div className="form-group">
-								<label htmlFor="inputAddress2">Address 2</label>
-								<input
-									type="text"
-									className="form-control"
-									id="inputAddress2"
-									placeholder="Apartment, studio, or floor"
+									id="inputAddress1"
+									placeholder="numero de calle o casa color"
 								/>
 							</div>
 							<div className="form-row">
-								<div className="form-group col-md-6">
-									<label htmlFor="inputCity">City</label>
-									<input type="text" className="form-control" id="inputCity" />
+								<div className="form-group col-md-4">
+									<label htmlFor="inputProvincia">Provincia</label>
+									<input type="text" className="form-control" id="inputProvincia" />
 								</div>
 								<div className="form-group col-md-4">
-									<label htmlFor="inputState">State</label>
-									<select id="inputState" className="form-control">
-										<option selected>Choose...</option>
-										<option>...</option>
-									</select>
+									<label htmlFor="inputCanton">Cantón</label>
+									<input type="text" className="form-control" id="inputCanton" />
 								</div>
-								<div className="form-group col-md-2">
-									<label htmlFor="inputZip">Zip</label>
-									<input type="text" className="form-control" id="inputZip" />
+								<div className="form-group col-md-4">
+									<label htmlFor="inputDistrito">Distrito</label>
+									<input type="text" className="form-control" id="inputDistrito" />
 								</div>
 							</div>
 							<div className="form-group">
